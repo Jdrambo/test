37bbcdbfd98311c1689099dde2cabcd39fe2d5c9
Johann de Rus <jdrambo@hotmail.fr>
2015-05-05 09:20:32 +0200

    Update main_out.js

--- main_out.js
+++ main_out.js
@@ -63,8 +63,8 @@
     }
 
     function T() {
-        L = (J - n / 2) * 1/*/ s + w*/;
-        M = (K - p / 2) * 1/*/ s + x*/
+        L = (J - n / 2) / s + w;
+        M = (K - p / 2) / s + x
     }
 
     function ea() {
